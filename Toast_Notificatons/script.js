0c8ac2fc29f04bb16136b19deb3fae15a2c9ff5f
Deepak Kumar <ak199638@gmail.com>
2024-02-28 11:51:58 +0530

    Toast_Notificatons

---
+++ Toast_Notificatons/script.js
@@ -0,0 +1,24 @@
+let tostBox = document.getElementById('toastBox');
+let successMsg ='<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg ='<i class="fa-solid fa-circle-xmark"></i> Please fix the error';
+let InvalidMsg ='<i class="fa-solid fa-circle-exclamation"></i>Invalid input check again';
+
+function showToast(msg){
+let toast = document.createElement('div');
+
+toast.classList.add ('toast');
+
+toast.innerHTML = msg;
+tostBox.appendChild(toast)
+
+if(msg.includes('error')){
+    toast.classList.add('error');
+}
+    if(msg.includes('Invalid')){
+        toast.classList.add('Invalid');
+}
+
+setTimeout(()=>{ 
+    toast.remove();
+},5000);
+}
