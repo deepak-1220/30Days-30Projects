cf9becb58213ca44b13f2803ab090e43bef230ae
Deepak Kumar <ak199638@gmail.com>
2024-03-03 00:53:58 +0530

    pop-upMsg

---
+++ pop-upMsg/script.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById("popup");
+let closepopup = document.getElementById("closepopup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+
+}
